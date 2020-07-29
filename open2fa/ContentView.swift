6b4e790c000a3462a54f4a7dbf03ddd6ccb1efe1
AginS <agins.main@gmail.com>
2020-07-29 13:14:26 +0300

    added "add codes" hint

--- open2fa/ContentView.swift
+++ open2fa/ContentView.swift
@@ -14,21 +14,35 @@ struct ContentView: View {
     
     @State private var showSheet = false
     @State private var isActive = false
+    @State private var rect: CGRect = CGRect()
+    
     var body: some View {
+        GeometryReader { geo in
         NavigationView {
-            List {
-                ForEach (core_driver.codes) { c in
-                     CodePreview(code: c, timeRemaning: self.core_driver.timeRemaning)
+            ZStack {
+                List {
+                    ForEach (core_driver.codes) { c in
+                         CodePreview(code: c, timeRemaning: self.core_driver.timeRemaning)
+                    }
                 }
+                .navigationBarTitle("Open 2FA")
+                .navigationBarItems(
+                    leading:
+                        NavigationLink(destination: PreferencesView().environmentObject(self.core_driver), label: { Text("Preferences") }),
+                    trailing:
+                        NavigationLink(destination: AddCodeView().environmentObject(self.core_driver), label: { Text("Add") }) )
+                
+                if core_driver.codes.count == 0 {
+                    Text("Add your codes using the button above")
+                        .multilineTextAlignment(.center)
+                        .font(.title)
+                        .foregroundColor(.secondary)
+                }
+                
             }
-            .navigationBarTitle("Open 2FA")
-            .navigationBarItems(
-                leading:
-                    NavigationLink(destination: PreferencesView().environmentObject(self.core_driver), label: { Text("Preferences") }),
-                trailing:
-                    NavigationLink(destination: AddCodeView().environmentObject(self.core_driver), label: { Text("Add") }) )
         }
         .navigationViewStyle(StackNavigationViewStyle())
+        }
     }
 }
 
