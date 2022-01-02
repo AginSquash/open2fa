cb4a83c6c5801527dd1b3512df4db5f9dad53dd4
AginS <agins.main@gmail.com>
2022-01-02 05:17:24 +0300

    fixed animation in iOS 15

--- open2fa/Views/TimeLeftView.swift
+++ open2fa/Views/TimeLeftView.swift
@@ -11,28 +11,39 @@ import SwiftUI
 struct TimeLeftView: View {
     let progress: CGFloat
     
-        var body: some View {
-            ZStack {
-                Circle()
-                    .stroke(lineWidth: 5.0)
-                    .opacity(0.3)
-                    .foregroundColor(Color.secondary)
-                
-                Circle()
-                    .trim(from: 0, to: progress)
-                    .stroke(style: StrokeStyle(lineWidth: 5.0, lineCap: CGLineCap.round))
-                    .rotationEffect(.degrees(-90))
-                    .foregroundColor( progress > 0.17 ? Color.orange : Color.red)
-                    .animation(.linear)
-                    
-            }
+    var currentColor: Color {
+        if #available(iOS 15.0, *) { /// bug in ios 15? (trim + stroke + dynamic color change)
+            return Color.orange
         }
+        
+        if progress > 0.17 {
+            return Color.orange
+        }
+        return Color.red
+    }
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 5.0)
+                .opacity(0.3)
+                .foregroundColor(Color.secondary)
+                .animation(.none)
+            
+            Circle()
+                .trim(from: 0, to: progress)
+                .stroke(currentColor, style: StrokeStyle(lineWidth: 5.0, lineCap: CGLineCap.round))
+                .animation(.linear)
+                .rotationEffect(.degrees(-90))
+                
+        }
+    }
     
 }
 
 struct TimeLeftView_Previews: PreviewProvider {
     static var previews: some View {
-        TimeLeftView(progress: 0.4)
+        TimeLeftView(progress: 0.2)
             .frame(width: 30, height: 30, alignment: .center)
             .previewLayout(.fixed(width: 40, height: 40))
     }
