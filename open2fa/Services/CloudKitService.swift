c67fb7c32f92e38d7616a280cdca52389f7e122d
AginS <agins.main@gmail.com>
2024-04-28 12:16:16 +0300

    Added checkAccountStatus & save & fetch PublicEncryptData

--- open2fa/Services/CloudKitService.swift
+++ open2fa/Services/CloudKitService.swift
@@ -9,11 +9,13 @@
 import Foundation
 import CloudKit
 
+class CloudKitService {
+    private static let accountObjectZoneName: String = "AccountObjectsZone"
     
     static func createZone() {
         let cloudContainer = CKContainer.default()
         let privateDatabase = cloudContainer.privateCloudDatabase
-        let zone = CKRecordZone(zoneName: CloudService.zoneName)
+        let zone = CKRecordZone(zoneName: CloudKitService.accountObjectZoneName)
         let saveOperation = CKModifyRecordZonesOperation(recordZonesToSave: [zone])
         privateDatabase.add(saveOperation)
     }
@@ -21,8 +23,23 @@ import CloudKit
     static func deleteZone() {
         let cloudContainer = CKContainer.default()
         let privateDatabase = cloudContainer.privateCloudDatabase
-        let zone = CKRecordZone(zoneName: CloudService.zoneName)
+        let zone = CKRecordZone(zoneName: CloudKitService.accountObjectZoneName)
         let deleteOperation = CKModifyRecordZonesOperation(recordZoneIDsToDelete: [zone.zoneID])
         privateDatabase.add(deleteOperation)
+    }
+    
+    func checkAccountStatus() async throws -> CKAccountStatus {
+        try await CKContainer.default().accountStatus()
+    }
+    
+    func save(_ record: CKRecord) async throws {
+        try await CKContainer.default().privateCloudDatabase.save(record)
+    }
+    
+    func fetchPublicEncryptData() async throws -> [PublicEncryptData] {
+        let query = CKQuery(recordType: PublicEncryptData.RecordKeys.type.rawValue, predicate: NSPredicate(value: true))
+        let result = try await CKContainer.default().privateCloudDatabase.records(matching: query)
+        let records = result.matchResults.compactMap({ try? $0.1.get() })
+        return records.compactMap(PublicEncryptData.init)
     }
 }
