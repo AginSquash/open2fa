1f019a67e50735ca2e0c4760de641c14ad618397
AginS <agins.main@gmail.com>
2024-04-28 12:15:52 +0300

    Added PublicEncryptData

---
+++ open2fa/Models/PublicEncryptData.swift
@@ -0,0 +1,44 @@
+//
+//  PublicEncryptData.swift
+//  open2fa
+//
+//  Created by Vlad Vrublevsky on 28.04.2024.
+//  Copyright © 2024 Vlad Vrublevsky. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+struct PublicEncryptData: Hashable {
+    enum RecordKeys: String {
+        case type = "PublicEncryptData"
+        case salt
+        case iv
+        case kvc
+    }
+    
+    let salt: String
+    let iv: [UInt8]
+    let kvc: [UInt8]
+}
+
+extension PublicEncryptData {
+    var record: CKRecord {
+        let record = CKRecord(recordType: Self.RecordKeys.type.rawValue)
+        record[Self.RecordKeys.salt.rawValue] = salt
+        record[Self.RecordKeys.iv.rawValue] = iv
+        record[Self.RecordKeys.kvc.rawValue] = kvc
+        return record
+    }
+}
+
+extension PublicEncryptData {
+    init?(from record: CKRecord) {
+        guard
+            let salt = record[Self.RecordKeys.salt.rawValue] as? String,
+            let iv = record[Self.RecordKeys.iv.rawValue] as? [UInt8],
+            let kvc = record[Self.RecordKeys.kvc.rawValue] as? [UInt8]
+        else { return nil }
+        self = .init(salt: salt, iv: iv, kvc: kvc)
+    }
+}
