71134c90705476980a976ca87d63b8e446cd45ef
AginS <agins.main@gmail.com>
2024-05-01 05:07:16 +0300

    Updated animation

--- open2fa/Views/TimeLeftView.swift
+++ open2fa/Views/TimeLeftView.swift
@@ -12,14 +12,14 @@ struct TimeLeftView: View {
     let progress: CGFloat
     
     var currentColor: Color {
-        if #available(iOS 15.0, *) { /// bug in ios 15? (trim + stroke + dynamic color change)
-            return Color.orange
-        }
-        
         if progress > 0.17 {
             return Color.orange
         }
         return Color.red
+    }
+    
+    var isInverted: CGFloat {
+        progress == 1 ? -1 : 1
     }
     
     var body: some View {
@@ -28,14 +28,14 @@ struct TimeLeftView: View {
                 .stroke(lineWidth: 5.0)
                 .opacity(0.3)
                 .foregroundColor(Color.secondary)
-                .animation(.none)
             
             Circle()
                 .trim(from: 0, to: progress)
                 .stroke(currentColor, style: StrokeStyle(lineWidth: 5.0, lineCap: CGLineCap.round))
-                .animation(.linear)
+                .animation(.linear, value: progress)
                 .rotationEffect(.degrees(-90))
-                
+                .scaleEffect(x: isInverted)
+                .animation(.none, value: isInverted)
         }
     }
     
@@ -43,7 +43,7 @@ struct TimeLeftView: View {
 
 struct TimeLeftView_Previews: PreviewProvider {
     static var previews: some View {
-        TimeLeftView(progress: 0.2)
+        TimeLeftView(progress: 0.8)
             .frame(width: 30, height: 30, alignment: .center)
             .previewLayout(.fixed(width: 40, height: 40))
     }
